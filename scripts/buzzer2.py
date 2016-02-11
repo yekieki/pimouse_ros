18c2fd0c199cb154ece7ef4902fc8dd35c989243
yasu ekimoto <eki.party@gmail.com>
2016-02-11 21:00:27 +0000

    Add a buzzer2.py

---
+++ scripts/buzzer2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import rospy
+form std_msgs.msg import UInt16
+
+def recv_buzzer(data):
+	rospy.loginfo(type(data))
+	rospy.loginfo(data.data)
+
+if __ name__ == '__main__':
+	rospy.init_node('buzzer')
+	rospy.Subscriber("buzzer", UIni16, recv_buzzer)
+	rospy.spin()
