930032d60c53e5b896efcc1bf469c1af5d04cfd4
yasu ekimoto <eki.party@gmail.com>
2016-02-11 21:17:05 +0000

    Add a buzzer2.py

--- scripts/buzzer2.py
+++ scripts/buzzer2.py
@@ -6,7 +6,7 @@ def recv_buzzer(data):
 	rospy.loginfo(type(data))
 	rospy.loginfo(data.data)
 
-if __ name__ == '__main__':
+if __name__ == '__main__':
 	rospy.init_node('buzzer')
 	rospy.Subscriber("buzzer", UIni16, recv_buzzer)
 	rospy.spin()
